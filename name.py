8cb8feb393ba9d14a1997f237333110d1e82ab6f
ABLREY <abielreyes@berkeley.edu>
2025-02-07 16:01:42 -0800

    Update name.py

--- name.py
+++ name.py
@@ -11,9 +11,9 @@ bme680.sea_level_pressure = 1013.25
 
 now = time.time()
 duration = 5
+curr = time.ctime()
 
-while time.time() - now < duration:
-    curr = time.ctime() 
+while time.time() - now < duration: 
     print("Current time:", curr , "\nTemperature: %0.1f C" % bme680.temperature ,  "Gas: %d ohm" % bme680.gas ,  "Humidity: %0.1f %%" % bme680.relative_humidity ,  "Pressure: %0.3f hPa" % bme680.pressure ,  "Altitude = %0.2f meters" % bme680.altitude)
     time.sleep(2)
     
