0121189aaf5e5b6167ef2466d72e31c27af09e4f
ABLREY <abielreyes@berkeley.edu>
2025-02-21 15:41:33 -0800

    Update test_inputs.py

--- test_inputs.py
+++ test_inputs.py
@@ -21,4 +21,22 @@ while count < run_time:
   print(count)
   count +=1
   time.sleep(1)
-        
+
+
+
+
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()   # uses board.SCL and board.SDA
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+# change this to match the location's pressure (hPa) at sea level
+bme680.sea_level_pressure = 1013.25
+
+now = time.time()
+duration = 5
+
+while time.time() - now < duration: 
+    curr = time.ctime()  
+    print(f"\rCurrent time: {curr} | Temperature: {bme680.temperature:.1f} C | Gas: {bme680.gas} ohm | Humidity: {bme680.relative_humidity:.1f}% | Pressure: {bme680.pressure:.3f} hPa | Altitude: {bme680.altitude:.2f} meters")
+    time.sleep(2)
+    
