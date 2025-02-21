af4a6dd982ac69af3b77eed88dbbc8c8ce88124d
ABLREY <abielreyes@berkeley.edu>
2025-02-21 15:42:50 -0800

    Update test_inputs.py

--- test_inputs.py
+++ test_inputs.py
@@ -29,14 +29,20 @@ while count < run_time:
 i2c = board.I2C()   # uses board.SCL and board.SDA
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
+
 # change this to match the location's pressure (hPa) at sea level
 bme680.sea_level_pressure = 1013.25
+
+# Set up the UART connection
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+pm25 = PM25_UART(uart, reset_pin=None)
+print("Found PM2.5 sensor, reading data...")
 
 now = time.time()
 duration = 5
 
-while time.time() - now < duration: 
-    curr = time.ctime()  
-    print(f"\rCurrent time: {curr} | Temperature: {bme680.temperature:.1f} C | Gas: {bme680.gas} ohm | Humidity: {bme680.relative_humidity:.1f}% | Pressure: {bme680.pressure:.3f} hPa | Altitude: {bme680.altitude:.2f} meters")
-    time.sleep(2)
+
+curr = time.ctime()  
+print(f"\rCurrent time: {curr} | Temperature: {bme680.temperature:.1f} C | Gas: {bme680.gas} ohm | Humidity: {bme680.relative_humidity:.1f}% | Pressure: {bme680.pressure:.3f} hPa | Altitude: {bme680.altitude:.2f} meters")
+time.sleep(2)
     
