fb0554260ec6b86176627071bb8916bee2667769
ABLREY <abielreyes@berkeley.edu>
2025-02-21 15:11:08 -0800

    Update test_inputs.py

--- test_inputs.py
+++ test_inputs.py
@@ -1,5 +1,8 @@
 import sys
 import time
+import adafruit_bme680
+
+import board
 
 print(sys.argv)
 
@@ -79,3 +82,25 @@ with open(csv_filename, mode="w", newline="") as csvfile:
         print("---------------------------------------")
 
 print(f"Data logging complete! File saved as {csv_filename}")
+
+
+
+
+
+
+
+
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()   # uses board.SCL and board.SDA
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+# change this to match the location's pressure (hPa) at sea level
+bme680.sea_level_pressure = 1013.25
+
+now = time.time()
+duration = 5
+
+while time.time() - now < duration: 
+    curr = time.ctime()  
+    print(f"\rCurrent time: {curr} | Temperature: {bme680.temperature:.1f} C | Gas: {bme680.gas} ohm | Humidity: {bme680.relative_humidity:.1f}% | Pressure: {bme680.pressure:.3f} hPa | Altitude: {bme680.altitude:.2f} meters")
+    time.sleep(2)
